349f99e46f0a61ee5f58c13e947bee4f3230204c
dddkhoa <dddkhoa.vn@gmail.com>
2023-06-10 00:44:25 +0700

    feat: Stratego env

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# Project: Recursive Belief Learning for Battleship game
+# Project: Recursive Belief Learning for Stratego game
+
+## Install requirements
+```text
+pip install -r requirements.txt
+```
+
+## Install `pre-commit` hooks for PEP8 formatting
+```text
+pre-commit install
+```
---
+++ src/stratego_env/Game.py
@@ -0,0 +1,121 @@
+import numpy as np
+from stratego_multiagent_env import *
+
+
+class Game:
+    """
+    This class specifies the base Game class. To define your own game, subclass
+    this class and implement the functions below. This works when the game is
+    two-player, adversarial and turn-based.
+
+    Use 1 for player1 and -1 for player2.
+    """
+
+    def __init__(self, env_config=None):
+        self.env = StrategoMultiAgentEnv(env_config=env_config)
+
+    def getInitBoard(self):
+        """
+        Returns:
+            startBoard: a representation of the board (ideally this is the form
+                        that will be the input to your neural network)
+        """
+        return self.env.random_initial_state_fn()
+
+    def getBoardSize(self):
+        """
+        Returns:
+            (x,y): a tuple of board dimensions
+        """
+        return self.env.base_env.rows, self.env.base_env.columns
+
+    def getActionSize(self):
+        """
+        Returns:
+            actionSize: number of all possible actions
+        """
+        return len(self.env.action_space)
+
+    def getNextState(self, board, player, action):
+        """
+        Input:
+            board: current board
+            player: current player (1 or -1)
+            action: action taken by current player
+
+        Returns:
+            nextBoard: board after applying action
+            nextPlayer: player who plays in the next turn (should be -player)
+        """
+        # ----------------------- TO BE IMPLEMENTED ----------------------------------
+
+        obs, reward, done, info = self.env.step(action)
+
+        return obs, list(obs.keys())[0]
+
+    def getValidMoves(self, board, player):
+        """
+        Input:
+            board: current board
+            player: current player
+
+        Returns:
+            validMoves: a binary vector of length self.getActionSize(), 1 for
+                        moves that are valid from the current board and player,
+                        0 for invalid moves
+        """
+        return self.env.base_env.get_valid_moves_as_1d_mask(board, player)
+
+    def getGameEnded(self, board, player):
+        """
+        Input:
+            board: current board
+            player: current player (1 or -1)
+
+        Returns:
+            r: 0 if game has not ended. 1 if player won, -1 if player lost,
+               small non-zero value for draw.
+
+        """
+        return self.env.base_env.get_game_ended(board, player)
+
+    def getCanonicalForm(self, board, player):
+        """
+        Input:
+            board: current board
+            player: current player (1 or -1)
+
+        Returns:
+            canonicalBoard: returns canonical form of board. The canonical form
+                            should be independent of player. For e.g. in chess,
+                            the canonical form can be chosen to be from the pov
+                            of white. When the player is white, we can return
+                            board as is. When the player is black, we can invert
+                            the colors and return the board.
+        """
+        return player * board
+
+    def getSymmetries(self, board, pi):
+        """
+        Input:
+            board: current board
+            pi: policy vector of size self.getActionSize()
+
+        Returns:
+            symmForms: a list of [(board,pi)] where each tuple is a symmetrical
+                       form of the board and the corresponding pi vector. This
+                       is used when training the neural network from examples.
+        """
+        pass
+
+    def stringRepresentation(self, board):
+        """
+        Input:
+            board: current board
+
+        Returns:
+            boardString: a quick conversion of board to a string format.
+                         Required by MCTS for hashing.
+        """
+        board_s = "".join(self.env.square_content[square] for row in board for square in row)
+        return board_s
